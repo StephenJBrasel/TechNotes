0823b0419ad4c2978777ac545a985e0da2e9a367
Stephen Brasel <sbrasel@pangiamlabs.com>
2021-05-27 16:39:59 -0400

    feat(Config): Git commit streamlining
    - [x] addded git-commit.json: 3 git commit snippets
    - [x] modified git_setup batch for clarity
    - [x] added new programming paradigm: unsynchronized

--- Config/git_setup.bat
+++ Config/git_setup.bat
@@ -29,11 +29,42 @@ git config --global fetch.prune true
 :: set --color-words as the default when showing differences.
 git config --global color.diff always
 
+::region Optional Destructive Settings
+:: Remove the @REM's in a region to uncomment the code, 
+:: Copy and paste that code into a LINUX terminal (not Command Prompt or Powershell) (until I figure out how to run this in cmd),
+:: Then re-comment the commands.
+
+::region Global GitIgnore
+:: the following creates a global-level .gitignore that contributes to every gitignore in the system
+:: useful if multiple projects need to ignore the same files, like .idea in Intellij or .vscode
+:: WARNING: This will override the ~/.gitignore file if any exists.
+
+@REM cd ~
+@REM ignore= "# Personal Settings
+@REM .vscode
+@REM .idea"
+@REM echo "$ignore" > .gitignore
+@REM git config --global core.excludesfile "~/.gitignore"
+::endregion Global GitIgnore
+
+
+::region Commit Message Template
+:: the following configures a default template for every git commit
+:: WARNING: This will override the ~/.gitmessage.git-commit file if any exists.
+
+@REM cd ~
+@REM template="type(scope): subject #meta [keep header<50chars]
+
+@REM multi-line description of the details of changes
+@REM - [ ] If practical, use markdown
+@REM See (Git Commit Message Guidelines)[https://gist.github.com/abravalheri/34aeb7b18d61392251a2] for examples.
+
+@REM [Progresses/Closes/etc.] Task_Reference"
+@REM echo "$template" > .gitmessage.git-commit
+@REM git config --global commit.template ~/.gitmessage.git-commit
+::endregion Commit Message Template
+
+::endregion Optional Destructive Settings
+
 :: show the configuration
 git config --list
-
-:: the following is optional for creating a gitignore in the User directory that contributes to every gitignore in the system
-:: useful if multiple projects need to ignore the same files, like .idea in Intellij or .vscode
-:: cd ~
-:: echo > .gitignore
-:: git config --global core.excludesfile "~/.gitignore"
